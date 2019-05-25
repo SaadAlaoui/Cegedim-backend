aa84c5024979a44dd33a7654cd59e9246e337670
Mohammed Saad Azizi Alaoui <m.saad.alaoui@gmail.com>
2019-05-25 11:52:18 +0000

    RequestMapping "/" can transform json object to Post object

--- src/main/java/com/cegedim/backend/MainController.java
+++ src/main/java/com/cegedim/backend/MainController.java
@@ -1,15 +1,37 @@
 package com.cegedim.backend;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.json.JSONArray;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainController {
-
 	private String url = "https://jsonplaceholder.typicode.com/posts";
 	
 	@RequestMapping("/")
-	public String defaultAction() {
-		return "Hello world !!";
+	public List<Post> defaultAction() {
+		List<Post> JsonList = new ArrayList<Post>();
+		JSONArray arr = new JSONArray("[\r\n" + 
+				"  {\r\n" + 
+				"    \"userId\": 1,\r\n" + 
+				"    \"id\": 1,\r\n" + 
+				"    \"title\": \"sunt aut facere repellat provident occaecati excepturi optio reprehenderit\",\r\n" + 
+				"    \"body\": \"quia et suscipit\\nsuscipit recusandae consequuntur expedita et cum\\nreprehenderit molestiae ut ut quas totam\\nnostrum rerum est autem sunt rem eveniet architecto\"\r\n" + 
+				"  }]");
+		for (int i = 0; i < arr.length(); i++)
+		{
+			JsonList.add(
+					new Post(
+							(int)arr.getJSONObject(i).get("id"),
+							(int)arr.getJSONObject(i).get("userId"),
+							arr.getJSONObject(i).get("title").toString(),
+							arr.getJSONObject(i).get("body").toString()
+					));
+		}
+		return JsonList;
 	}
 }
