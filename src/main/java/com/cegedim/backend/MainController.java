eda7108a992007d1758cd0daeab31c7940410f1e
Mohammed Saad Azizi Alaoui <m.saad.alaoui@gmail.com>
2019-05-25 11:43:37 +0000

    Fisrt Run Application

--- src/main/java/com/cegedim/backend/MainController.java
+++ src/main/java/com/cegedim/backend/MainController.java
@@ -1,5 +1,13 @@
 package com.cegedim.backend;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class MainController {
-	
+
+	@RequestMapping("/")
+	public String defaultAction() {
+		return "Hello world !!";
+	}
 }
