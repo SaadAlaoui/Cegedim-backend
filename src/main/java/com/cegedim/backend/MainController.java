b825d7f03080cf2e8536fff2431fb160d933d550
Mohammed Saad Azizi Alaoui <m.saad.alaoui@gmail.com>
2019-05-25 11:46:27 +0000

    New dependency org.json Added !

--- src/main/java/com/cegedim/backend/MainController.java
+++ src/main/java/com/cegedim/backend/MainController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MainController {
 
+	private String url = "https://jsonplaceholder.typicode.com/posts";
+	
 	@RequestMapping("/")
 	public String defaultAction() {
 		return "Hello world !!";
